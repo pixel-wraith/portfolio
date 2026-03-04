06b202c35f596e86f53b54b69188593a5de4323a
pixel-wraith <allhailthewraith@pm.me>
2026-03-03 23:04:19 -0500

    feat: 👻 adds uses page

---
+++ src/routes/uses/+page.svelte
@@ -0,0 +1,195 @@
+<script>
+    import Intro from "$lib/components/Intro.svelte";
+    import Section from "$lib/components/Section.svelte";
+</script>
+
+<div class="container">
+    <Intro header="Tools and Gear">
+        <div class="intro-content row">
+            <div>
+                <p>
+                    This is a list of the hardware and software I'm currently using on a daily basis. I'm always on the lookout for
+                    tools that will help me be more productive and efficient, so if you have any suggestions, please let me know!
+                </p>
+
+                <p>
+                    Oh, and just a heads up, most of these are Amazon affiliate links, so be sure to click them and buy something
+                    so I can keep the lights on around here! 😆
+                </p>
+            </div>
+        </div>
+    </Intro>
+
+    <Section>
+        <h2>Hardware</h2>
+
+        <ul>
+            <li>
+                I recently updated my desk to a <a href="https://amzn.to/47abisC" target="_blank">HUANUO 48″ x 24″ Electric Standing Desk</a>
+                and I've been really happy with it. It's not as fancy as some of the other desks out there, but it's sturdy, has a good range of motion, and the price was right.
+            </li>
+            <li>
+                My desk chair is a <a href="https://secretlabchairs.ca/products/titan-evo-2022-series?sku=R22PU-Batman" target="_blank">Secretlab Titan Evo</a> emblazened
+                with the Batman logo with <a href="https://amzn.to/3Td6ZXE" target="_blank">clear roller blade wheels</a> that are super smooth, even on carpet.
+            </li>
+            <li>
+                I'm currently using a 2021 16" Macbook Pro M1 Max with 64 GB ram and a 2TB SSH. And of course, it's covered in stickers!
+            </li>
+            <li>
+                My external monitors are 2x <a href="https://amzn.to/4hlijed" target="_blank">Phillips 4K UHD Monitors</a>.
+            </li>
+            <li>
+                One of the best investments I ever made was splurging on a <a href="https://www.zsa.io/moonlander" target="_blank">Moonlander Mark I keyboard</a>. It's a bit
+                pricey, but has easily been the most comfortable and customizable keyboard I've ever used.
+            </li>
+            <li>
+                After a recommendation from one of my fav podcasters, I upgraded my mouse to a <a href="https://amzn.to/3WnXqXL" target="_blank">Logitech MX Master 3S</a> and
+                have been loving it!
+            </li>
+            <li>
+                I recently upgraded my microphone to a <a href="https://amzn.to/4hlijed" target="_blank">Shure MV6</a> with a
+                <a href="https://amzn.to/4alGiYk" target="_blank">Pneumatic Neewer Low Profile Mic Arm</a> and have been really happy with it.
+            </li>
+            <li>
+                I use a <a href="https://www.amazon.com/Logitech-Widescreen-Calling-Recording-Desktop/dp/B006JH8T3S?sr=8-1" target="_blank">Logitech HD Pro C920</a> webcam for
+                video calls and recording videos.
+            </li>
+            <li>
+                To light my office, I use 2 <a href="https://amzn.to/3ABCRPL" target="_blank">Neewer LED Panel Lights</a> that I bounce off the walls + multiple strips of <a href="https://www.amazon.com/dp/B07XBSP6BJ?ref_=ppx_hzsearch_conn_dt_b_fed_asin_title_5">Govee Color Changing LED Strip Lights</a> for background ambient lighting.
+            </li>
+            <li>
+                With all these devices, I needed something to keep them all connected without having to deal with a bunch of cables...not to mention there are only so many ports on a Macbook. So I picked up a
+                <a href="https://amzn.to/4cRDcen" target="_blank">StarTech.com Thunderbolt 3 to Dual DisplayPort Adapter</a>,
+                a <a href="https://amzn.to/3XauWjH" target="_blank">Selore&S-Global USB C to Dual HDMI Adapter</a>, and a
+                <a href="https://amzn.to/4g1zBgF" target="_blank">13 port LATORICE Powered USB Hub</a>
+                to plug them all into. Then those are all connected via an
+                <a href="https://amzn.to/4cQfrTK" target="_blank">OWC 5 Port Thunderbolt Hub</a>.
+                Now when I get to my desk, all I have to do is plug in 1 USB C cable and everything is good to go!
+            </li>
+            <li>
+                I've always hated having to carry around all kinds of bricks and adapters to plug in and charge stuff. I searched for a while
+                and nothing really worked great, but finally I found this
+                <a href="https://amzn.to/3Zaw8WS" target="_blank">Portable 7 Port Fast Charging Station</a>.
+                Now I can charge my laptop, phone, iPad, and anything else I need all at the same time.
+            </li>
+            <li>
+                I also tried a bunch of different USB C cables, but my favorite one by far has to be the
+                <a href="https://amzn.to/4cK6P1b" target="_blank">Dbilida 6.6ft Thunderbolt 3 Cable (40Gbps)</a>. I've
+                had it for a long time and it's still doing great!
+            </li>
+            <li>
+                I carry everything in a <a href="https://www.amazon.com/Mystery-Ranch-Blitz-Backpack-Organized/dp/B0GFB3NZJR/ref=sr_1_1" target="_blank">Mystery Ranch Blitz 35 Backpack</a>.
+            </li>
+            <li>
+                I listen to <a href="https://amzn.to/3yYjMXj" target="_blank">Beats Fit Pro</a> earbuds most of the time, whether I'm at my desk or
+                out and about.
+            </li>
+            <li>
+                But when I don't have my earbuds in, I have a <a href="https://www.amazon.com/dp/B0D4STD5ZC" target="_blank">Beats Pill - Portable Bluetooth Speaker</a> for when I want to jam out.
+            </li>
+            <li>
+                Another small but awesome little piece of gear is the <a href="https://www.amazon.com/dp/B0CBPS7GBP" target="_blank">ProCase Hard Travel Electronic Organizer Case</a>.
+                This thing is fantastic for storing, organizing, and protecting all my cables and accessories.
+            </li>
+            <li>
+                After a long search for the right power bank to carry with me, I finally found the
+                <a href="https://amzn.to/3AIFCym" target="_blank">Omni 20+ 20000mah Laptop Power Bank Portable Charger</a>. It's been the
+                perfect companion for my laptop and phone when I'm on the go.
+            </li>
+        </ul>
+    </Section>
+
+    <Section>
+        <h2>Software</h2>
+
+        <ul>
+            <li>
+                While I use multiple AI tools for various tasks, my AI companion of choice is <a href="https://claude.com/product/claude-code" target="_blank">Claude Code</a> for my daily work. I've spent lots of time building my own library of prompts, skills, and subagents to help me be more productive and efficient.
+            </li>
+            <li>
+                I use <a href="https://www.cursor.com/" target="_blank">Cursor</a> as my primary editor with the
+                <a href="https://marketplace.visualstudio.com/items?itemName=GitHub.github-vscode-theme" target="_blank">GitHub Dark Default</a> theme.
+                <br>
+                <br>
+                But I was a longtime Vim user too, so I still use that a good ammount (especially now that I'm in the terminal so much more with working with Claude). I have a custom theme called
+                <a href="https://github.com/iamthe-Wraith/breeze" target="_blank">Breeze</a> I use for Vim that's a fork of the
+                <a href="https://github.com/dracula/vim" target="_blank">Dracula</a> theme.
+            </li>
+            <li>
+                My termainl of choise is <a href="https://www.warp.dev/" target="_blank">Warp</a> I really love it.
+                It's a fantastic tool with so many little benefits I could (and do) go on about it for a while...but don't worry, I won't do that to you.
+            </li>
+            <li>
+                I finally gave up on <a href="https://arc.net/" target="_blank">Arc</a> since the company
+                is no longer actively developing it 😢, so now my primary browser is <a href="https://vivaldi.com" target="_blank">Vivaldi</a>, which is highly customizable and has a lot of features I love.
+            </li>
+            <li>
+                I replaced Mac's built in Spotlight with <a href="https://www.raycast.com/" target="_blank">Raycast</a> (Pro version) for quick and easy access to
+                multiple AI models and a bunch of plugins.
+            </li>
+            <li>
+                For note taking I use <a href="https://obsidian.md/" target="_blank">Obsidian</a> with a few different vaults to help keep things organized. Particularly
+                my "second brain", which is where I keep all my notes and ideas...a method I learned from one of my favorite books,
+                <a href="https://amzn.to/3ZbBhOr" target="_blank">How to Take Smart Notes</a>.
+            </li>
+            <li>
+                I've been using <a href="https://1password.com/" target="_blank">1Password</a> as my password manager for years now. It used to be LastPass, but I switched
+                a few years ago after some security concerns.
+            </li>
+            <li>
+                I've been a loyal <a href="https://proton.me/mail" target="_blank">Proton</a> customer for many years now. I use it for all my personal email,
+                encrypted cloud storage, and VPN.
+            </li>
+            <li>
+                I built <a href="https://buzybee.buzz" target="_blank">BuzyBee</a> to help myself manage all my tasks and projects using the
+                <a href="https://gettingthingsdone.com/" target="_blank">GTD Methodology</a>, and use it every day.
+            </li>
+        </ul>
+    </Section>
+
+    <p>
+        That's about it for now. I'll update this page as I find new tools and software that make my life easier and better.
+    </p>
+</div>
+
+<style>
+    .container {
+		width: 96%;
+		margin: 0 auto;
+		padding: 2rem 1rem 1rem;
+
+		& p {
+			margin-bottom: 2rem;
+		}
+	}
+
+	.row {
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+
+		& .img-container {
+			display: flex;
+			justify-content: center;
+		}
+
+		@media (min-width: 500px) {
+			flex-direction: row;
+			gap: 0.5rem;
+		}
+	}
+
+	ul {
+		margin-bottom: 2rem;
+		color: var(--primary-500);
+		list-style: square;
+	}
+
+	li {
+		margin: 1.25rem 0;
+	}
+
+	a {
+		color: var(--accent1-500);
+	}
+</style>
